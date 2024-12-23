308150b43ce36aed9993344004bc01436cc19d75
SUJITHK001 <kumarsujith672@gmail.com>
2024-12-23 10:04:45 +0530

    jave file is updated

---
+++ DBConnect.java
@@ -0,0 +1,10 @@
+welcome...!
+#welcome..!
+public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+    System.out.println(sum); // Print the sum of x + y
+  }
+}
